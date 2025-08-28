9fb4969d3cb4b23c2a425e29bc7fa12d3347fe18
Sam Newton <smnewton54@Florence.local>
2025-08-28 17:18:44 -0500

    chore: ci flutter test syntax

--- lib/core/theme/app_design_tokens.dart
+++ lib/core/theme/app_design_tokens.dart
@@ -407,7 +407,7 @@ class ProfessionalTheme {
               letterSpacing: 0.25,
               height: 1.5),
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 0, // We use custom shadows instead
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(AppDesignTokens.radiusLg),
